89ce6a60457ba81839d97860dc4dc8e56f84cd38
Ricardo Mazzali <rlmazzali@gmail.com>
2025-06-06 15:39:05 -0300

    Corrigido host 0.0.0.0 e porta dinâmica para Render

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Render define essa variável PORT automaticamente
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
