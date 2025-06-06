643ed613d8270bd4d32b9edcf4da10968a2a4d12
Ricardo Mazzali <rlmazzali@gmail.com>
2025-06-06 15:32:09 -0300

    Fix final: Bind host 0.0.0.0 e porta do Render

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))  # Render define essa variável PORT automaticamente
     app.run(host="0.0.0.0", port=port)
