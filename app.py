fafd4219d943590a46790d248fe8123c1472182c
Ricardo Mazzali <rlmazzali@gmail.com>
2025-06-06 17:18:33 -0300

    layout atualizado

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
+import json
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    with open("data/cotacoes.json", encoding="utf-8") as f:
+        cotacoes = json.load(f)
+    return render_template("index.html", cotacoes=cotacoes)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
